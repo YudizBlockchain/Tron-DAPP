d13432ab628ea811e63cb0b725588deed1dc29b8
meetsiraja <49753117+meetsiraja@users.noreply.github.com>
2019-04-18 17:01:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const TronWeb = require('tronweb');
+const express = require('express')
+var bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.use("/source",express.static(__dirname + "/source"));
+
+app.get('/', function (req, res) {
+    res.render("Index");
+});
+
+const fullNode = "https://api.shasta.trongrid.io"
+const solidityNode = "https://api.shasta.trongrid.io";
+const eventServer = "https://api.shasta.trongrid.io";
+const privateKey = 'b112cfa7a1b71b55fe9bff5d9b23557d4f87ad679a948ee1d27397212f21366f'; // Enter Private key of your account
+
+const tronWeb = new TronWeb(fullNode, solidityNode, eventServer, privateKey);
+
+tronWeb.setAddress('TQyZbpU3G7JZjxb2ioa42FRgKAD1Vj8kgR'); //Enter Address of your account
+
+let contractAddress = 'TAdfvC2yscBkhzokuWNbaHt33Gop7mQpQg' //Enter Your Smart Contract Contract Address
+let abi = [{"constant":false,"inputs":[{"name":"value","type":"string"}],"name":"postMessage","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getMessage","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"}]
+
+let instance = tronWeb.contract(abi, contractAddress);
+
+let args = {
+    callValue:0,
+    shouldPollResponse: true
+}
+
+app.post('/post', async function(req, res) {
+    let para = req.body.postmessage
+    try{
+        let connection = await instance.postMessage(para).send(args);
+        console.log(connection)
+        res.send({
+                'data':'success'
+        })
+    } catch(error){
+        console.log(error)
+    }
+});
+
+app.get('/get', async function(req, res) {
+    let connection = await instance.getMessage().call();
+    res.send(connection);
+});
+
+app.listen(8001, function(){
+    console.log("Server started at 8001...");
+}); 
